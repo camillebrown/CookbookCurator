6753dd4cfcaa1a59b7a6d16d60d2da6b2a3009de
“camillebrown” <camillebrown11@gmail.com>
2020-11-07 11:47:41 -0800

    all updates so far including login and logout alerts

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const ejsLayouts = require('express-ejs-layouts')
+const session = require('express-session')
+const passport = require('./config/ppConfig.js')
+const flash = require ('connect-flash') // MUST GO AFTER THE SESSION MIDDLEWARE
+
+// set ejs and ejs layouts
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+//body parser middleware (this makes req.body work)
+app.use(express.urlencoded({extended: false}))
+
+//set up session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+// set up passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//set up flash middleware
+app.use(flash())
+
+// CUSTOM MIDDLEWARE
+app.use((req, res, next) => {
+    // before every route, attach the flash messages and current user to the res.locals
+    // this will give us access to these values in all our ejs pages
+    res.locals.alerts = req.flash()
+    res.locals.currentUser = req.user
+    next() // move on to the next piece of middleware
+})
+
+// controllers middleware
+app.use('/auth', require('./controllers/auth.js'))
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+app.get('/profile', (req, res) =>{
+    res.render('profile')
+})
+
+app.listen(3000, ()=>{
+    console.log("Keep your heart 3 stacks")
+})
---
+++ readme.md
@@ -0,0 +1,22 @@
+ # Express Auth Boilerplate
+
+ * create a node app
+ * .gitignore (include node_modules)
+ * install and set up express (npm i express)
+ * test by setting up a home route and console.log for your port
+ * create a controllers folder and add auth.js
+ * set up controllers middleware
+ * within the controller, set up the router and middleware (```module.exports = router```)
+ * set up ejs and ejsLayouts along with their middleware
+ * create forms on both the signup + login pages (POST methods to match the post routes)
+ * use req.body to collect the inputted data
+ * NEED body parser middleware (```app.use(express.urlencoded({extended: false}))```)
+ * install sequelize - start with npm i sequelize pg
+ * initialize sequelize - sequelize init
+ * adjust your config file
+ * create a database (sequelize db:create [NAME OF DATABASE])
+ * create a model within the database (sequelize model:create --name --attributes)
+ * migrate the new model (sequelize db:migrate)
+
+
+# adding sequelize validations
